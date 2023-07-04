fa9f6251d659f3b8e6c30b87133cb32298f69e5a
Prabhath Madhushan <prabathmadushan5@gmail.com>
2023-07-04 09:20:45 +0530

    OV-61

--- src/lib/components/toolbar/navigation/navigation.tsx
+++ src/lib/components/toolbar/navigation/navigation.tsx
@@ -1,16 +1,22 @@
 import styles from "./navigation.module.scss";
 import { GrFormNext } from "react-icons/gr";
 
-interface IProps{
-  next:()=>void,
-  previous:()=>void
+interface IProps {
+  next: () => void;
+  previous: () => void;
 }
 
-const Navigation = (props:IProps) => {
-  return <div className={styles.container}>
-    <GrFormNext style={{transform: "rotate(180deg)"}} onClick={props.previous}/>
-    <GrFormNext  onClick={props.next}/>
-  </div>;
+const Navigation = (props: IProps) => {
+  return (
+    <div className={styles.container}>
+      <GrFormNext
+        style={{ transform: "rotate(180deg)" }}
+        onClick={props.previous}
+      />
+      <span>Week</span>
+      <GrFormNext onClick={props.next} />
+    </div>
+  );
 };
 
 export default Navigation;
